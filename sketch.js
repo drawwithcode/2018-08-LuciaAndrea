4cc688e4134bf3b5413829a856892bd2f0dc0daa
LuciaAndrea <43391254+LuciaAndrea@users.noreply.github.com>
2018-11-20 22:49:30 +0100

    Sensor exercise

--- sketch.js
+++ sketch.js
@@ -1,11 +1,52 @@
-function preload(){
-  // put preload code here
+
+
+//variabili
+
+var check = false;
+var tilt = false;
+var sea;
+
+
+function preload() {
+
+
+
+  seaOUT = loadImage("./assets/surface.png");
+  seaIN = loadImage("./assets/underwater.png");
 }
 
 function setup() {
-  // put setup code here
+
+  createCanvas(windowWidth, windowHeight);
+  sea = image(seaOUT, 0, 0, image.width, image.height);
 }
 
 function draw() {
-  // put drawing code here
+  fill('white');
+  noStroke();
+  textSize(30);
+  textAlign(CENTER);
+  text('Shake the device to dive into the sea', width/3, height / 4);
+
+  if (check == true) {
+    sea = image(seaIN, 0, 0, image.width, image.height);
+    text('Turn the device to breath over surface', width/3, height / 4);
+  } else if (tilt == true) {
+    sea = image(seaOUT, 0, 0, image.width, image.height);
+    text('Shake to go back underwater',width/3, height / 4);
+  }
+
 }
+//functions
+
+function deviceShaken() {
+  check = true;
+  tilt = false;
+}
+function deviceTurned() {
+
+  if (turnAxis === 'X') {
+    tilt = true;
+    check = false;
+  }
+  }
